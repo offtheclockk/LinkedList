87da2e75ea1dc6741a386bb934a5a8b1fb1e5853
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2023-03-17 14:03:46 -0700

    Implement toArray() method

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         var list = new LinkedList();
-        System.out.println(list.size());
         list.addLast(10);
-        System.out.println(list.size());
         list.addLast(20);
-        System.out.println(list.size());
         list.addLast(30);
-        list.removeFirst();
-        list.removeLast();
-        System.out.println(list.size());
-        list.removeLast();
+
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
     }
 }
